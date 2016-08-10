508bc389c93477556fb92d7f8c554cb5a6b26ff2
marc desrosiers <mdesrosiers@norauto.fr>
2016-08-10 17:34:08 +0200

    all is ok. Yes

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,12 +1,28 @@
 class ReviewsController < ApplicationController
-
-  def index
-  end
+  before_action :find_restaurant, only: [:new, :create]
 
   def new
+    @review = Review.new
   end
 
   def create
+    @review = @restaurant.reviews.new(review_params)
+    if @review.valid?
+      @review.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:content, :rating)
+  end
+
+  def find_restaurant
+    @restaurant = Restaurant.find(params[:restaurant_id])
   end
 
 end
